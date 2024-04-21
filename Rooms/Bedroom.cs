6449452c09ede68c36f3c4ac293dd9ab0a6cae2d
Ruth Garcia <ruthggarcia99@gmail.com>
2024-04-20 22:08:49 -0400

    Added a case called "mirror".

--- Rooms/Bedroom.cs
+++ Rooms/Bedroom.cs
@@ -6,10 +6,10 @@ namespace NarrativeProject.Rooms
     {
 
         internal override string CreateDescription() =>
-@"You wake up on the floor to a strange bedroom. 
+@"You wake up on the cold floor of a strange bedroom. 
 The room only contains a [bed], a [drawer], 
 and a broken [mirror]. There is a [teddy bear] nicely tucked in on the bed. 
-And you see the door in front of you leads to a [hallway]. 
+And you see the door in front of you that leads to a [hallway]. 
 ";
 
         internal override void ReceiveChoice(string choice)
@@ -18,13 +18,15 @@ And you see the door in front of you leads to a [hallway].
             {
                 case "drawer":
                     Console.WriteLine("You open the drawer, revealing a rusty key." +
-                        "its cold to the touch. You feel a chill run down your spine as you" +
-                        "pocket the key.");
+                        "\nIt's cold to the touch. " +
+                        "You feel a chill run down your spine" +
+                        " as you pocket the key.");
                     break;
 
-                case "bathroom":
-                    Console.WriteLine("You enter the bathroom.");
-                    Game.Transition<Bathroom>();
+                case "mirror":
+                    Console.WriteLine("As you approach the broken mirror," +
+                        "\n you accidently activate a trap!" +
+                        "\nYou are struck by flying glass fragments!");
                     break;
                 case "door":
                     if (!AtticRoom.isKeyCollected)
