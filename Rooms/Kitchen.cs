c4ea2c5857a8a48806684fb5f2e9de45147ea3f2
Ruth Garcia <ruthggarcia99@gmail.com>
2024-04-21 18:15:17 -0400

    Added a method called "InspectCupboard" .

--- Rooms/Kitchen.cs
+++ Rooms/Kitchen.cs
@@ -4,10 +4,10 @@ namespace NarrativeProject.Rooms
 {
     internal class Kitchen : Room
     {
-        internal static bool isKeyCollected;
+        
 
         internal override string CreateDescription() =>
-@"You step into the [kitchen], feeling the darkness of the [hallway] behind you loom ominously...
+@"You step into the kitchen, feeling the darkness of the [hallway] behind you loom ominously...
 You are greeted by a dusty table and a lonely [cupboard]. The rooms is filled
 with cobwebs on the falling ceiling.
 ";
@@ -16,18 +16,45 @@ with cobwebs on the falling ceiling.
         {
             switch (choice)
             {
-                case "bedroom":
-                    Console.WriteLine("You return to your bedroom.");
-                    Game.Transition<Bedroom>();
+                case "hallway":
+                    Console.WriteLine("You return to the creepy hallway.");
+                    Game.Transition<Hallway>();
                     break;
-                case "6969":
-                    Console.WriteLine("The chest opens and you get a key.");
-                    isKeyCollected = true;
+                case "cupboard":
+                    InspectCupboard();
                     break;
                 default:
                     Console.WriteLine("Invalid command.");
                     break;
             }
+        }
+        private void InspectCupboard()
+        {
+            Console.WriteLine("You approach the locked cupboard. It has a note on it.");
+
+            Console.WriteLine("\n\"To see what's inside, arrange the utensils on the table" +
+                "\nin alphabetical order.\"");
+            Console.WriteLine("What do you want to do? ");
+            Console.WriteLine("1. Attempt arranging the ustensils.");
+            Console.WriteLine("2. Do nothing.");
+            string choice = Console.ReadLine(); 
+
+            switch(choice)
+            {
+                case "1":
+                    ArrangeUstensils();
+                    break;
+                case "2":
+                    Console.WriteLine("You decide to do nothing.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+            }
+        }
+        private void ArrangeUstensils()
+        {
+
         }
     }
 }
