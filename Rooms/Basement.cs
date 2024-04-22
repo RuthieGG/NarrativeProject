2dc0ef91ce6a9743abe49f5fb92dad4107b9a58a
Ruth Garcia <ruthggarcia99@gmail.com>
2024-04-22 18:20:21 -0400

    Added a description for the "Basement"

--- Rooms/Basement.cs
+++ Rooms/Basement.cs
@@ -5,7 +5,10 @@ namespace NarrativeProject.Rooms
     internal class Basement : Room
     {
         internal override string CreateDescription() =>
-@"
+@"Descending into the basement, 
+the air grows colder, and the sound of dripping water echoes off the stone walls.
+A door ahead catches your eyes. It outlines faintly some light.
+Beyond lies the promise of escape, the sunny windy air whispering of freedom!
 ";
 
         internal override void ReceiveChoice(string choice)
