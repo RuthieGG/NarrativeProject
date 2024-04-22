e6cc1288a97298f6ab587fceca13847c36cef78d
Ruth Garcia <ruthggarcia99@gmail.com>
2024-04-21 22:10:11 -0400

    Added room called "LastRoom".

---
+++ Rooms/LastRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Rooms
+{
+    internal class LastRoom : Room
+    {
+        internal override string CreateDescription() =>
+           @"";
+            internal override  void ReceiveChoice(string choice)
+        {
+
+        }
+    }
+}
