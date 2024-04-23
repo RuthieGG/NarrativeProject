f065643367a5f61e1a320b8a31ca0f8a488d2478
Ruth Garcia <ruthggarcia99@gmail.com>
2024-04-23 12:55:19 -0400

    Added description for "Last Room".

--- Rooms/LastRoom.cs
+++ Rooms/LastRoom.cs
@@ -10,7 +10,10 @@ namespace NarrativeProject.Rooms
     {
         internal override string CreateDescription() =>
            @"You open the door, expecting to be greeted by freedom.
-Instead, your find yourself in a  ";
+Instead, your find yourself in a strange chamber. The illusion of
+the outside fades...
+At the center of the room, there is a pedestal. Its missing a piece.
+It seems like something needs to be placed to finally escape...";
             internal override  void ReceiveChoice(string choice)
         {
             switch(choice)
